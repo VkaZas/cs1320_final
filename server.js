3346131297ee0b6b7d3068e26bc0f56169c86a6f
Yelena Denisenko <yelena_denisenko@brown.edu>
2018-04-19 18:50:28 -0400

    createEvent post method filled out

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const colors = require('colors');
 const babel = require('@babel/core');
 const index = require('./routes/index'); 
+const uuidv4 = require('uuid/v4');
+var buildUrl = require('build-url');
+var nodemailer = require('nodemailer');
 
 // View engine
 app.set('views', path.join(__dirname, 'views'));
@@ -22,13 +25,13 @@ var connEvents = anyDB.createConnection('sqlite3://events.db');
 var connTimeSlots = anyDB.createConnection('sqlite3://time_slots.db');
 
 var events = "CREATE TABLE events ( \
-  id INTEGER PRIMARY KEY AUTOINCREMENT, \
+  id TEXT PRIMARY KEY, \
   name TEXT, \
   creator_email, TEXT\
   start_date, DATE \
   end_date, DATE \
-  start_time, TIME \
-  end_time, TIME \
+  start_time_list, TEXT \
+  end_time_list, TEXT \
   attendents, TEXT \
   locations TEXT, \
   location_votes TEXT \
@@ -39,10 +42,18 @@ var timeSlots = "CREATE TABLE time_slots (\
   event_id TEXT, FOREIGN KEY REFERENCES events(id) \
   name, TEXT \
   date, DATE \
-  time_start, INT \
-  time_end, INT \
+  slotScore, TEXT \
 )";
 connTimeSlots.query(timeSlots, function(err, data) {});
+
+//Email setup
+var transporter = nodemailer.createTransport({
+  service:' gmail',
+  auth: {
+    user: 'meetng132@gmail.com',
+    pass: 'meetng1322018'
+  }
+});
 
 // Routes
 app.use('/', index);
@@ -52,27 +63,54 @@ app.get('/event/create', function(request, response) {
 });
 
 app.post('/event/createEvent', function(request, response) {
+  var id = uuidv4()
   var event_name = request.params.event_name;
   var creator_email = request.params.creator_email;
   var start_date = request.params.start_date;
   var end_date = request.params.end_date;
   var locations = request.params.locations;
-  var createNewEvent = "INSERT INTO events (event_name, creator_email, start_date, end_date, locations) VALUES($1,$2,$3,$4,$5)";
-  conn.query(createNewEvent,[name,creator_email,start_date,end_date,locations],function(err,data) {
+  var createNewEvent = "INSERT INTO events (id, event_name, creator_email, start_date, end_date, locations) VALUES($1,$2,$3,$4,$5)";
+  conn.query(createNewEvent,[id,name,creator_email,start_date,end_date,locations],function(err,data) {
     if (err) {
-      //do something about err here
+      console.log(err);
+      //maybe alert the user and redirect somewhere?
     } else {
-      //create link to emit to client side 
+      //create link to emit to email to the creator
+      //how are we doing this - locally or serving this on aws or something like that? 
+      var decision_url = buildUrl('http://localhost:8080', {
+        path: 'event/' + id + '/decide'
+      });
+      //email content set up
+      var mailoptions = {
+        from: 'meetng132@gmail.com',
+        to: creator_email,
+        subject: 'Link to Decision Page for ' + event_name,
+        text: 'Here\'s your link! ' + decision_url
+      };
+      //url we're sending to the creator so they can send it to attendees
+      var attend_url = buildUrl('hhtp://localhost:8080', {
+        path: 'attend/' + id
+      });
+      //send email to creator with link to 
+      transporter.sendMail(mailoptions, function(err, data) {
+        if (err) {
+          console.log(err);
+          io.sockets.in(id).emit('url', attend_url, decision_url);
+        } else {
+          io.sockets.in(id).emit('url', attend_url);
+        }
+      });
     }
   });
 });
 
 app.get('/attend/:id', function(request, response) {
+  var id = request.params.id;
 	response.render('create');
 });
 
 app.post('/attend/:id', function(request, resposne) {
-  //need to flesh this out some more
+  
 });
 
 app.post("/attend/:id/updatetimeslot", function(request, resposne) {
